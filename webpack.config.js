fd9679ce0a8df92dee2b070db69a085399bcaac0
Cesar <cesarnavarrete1337@gmail.com>
2018-06-17 14:33:03 -0400

    Soporte para CSS añadido

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  { loader: "style-loader" },
+                  { loader: "css-loader" }
+                ]
             }
         ]
     },
